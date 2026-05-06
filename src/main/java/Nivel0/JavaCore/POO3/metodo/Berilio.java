3030a6bf5a93b4cc0a80777097b97666c02a5911
DustCore <contagamera2014@gmail.com>
2026-05-06 10:53:17 -0300

    Atualização de Sei lá oq

--- src/main/java/Nivel0/JavaCore/POO3/metodo/Berilio.java
+++ src/main/java/Nivel0/JavaCore/POO3/metodo/Berilio.java
@@ -5,4 +5,6 @@ public class Berilio extends Elemento {
     public Berilio(String nome, int elemento, int reacao, int durabilidade) {
         super(nome, elemento, reacao, durabilidade);
     }
+
+    // preciso estudar química básica e ver o que cada um faz e coloca aqui!
 }
--- src/main/java/Nivel0/JavaCore/POO3/metodo/Hidrogenio.java
+++ src/main/java/Nivel0/JavaCore/POO3/metodo/Hidrogenio.java
@@ -5,4 +5,6 @@ public class Hidrogenio extends Elemento {
     public Hidrogenio(String nome, int elemento, int reacao, int durabilidade) {
         super(nome, elemento, reacao, durabilidade);
     }
+
+    // preciso estudar química básica e ver o que cada um faz e coloca aqui!
 }
